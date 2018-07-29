c8e73c7a440179bbf1e6fcc606089b84c2e854aa
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-07-29 17:39:27 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -7,19 +7,83 @@ $(document).ready(function() {
     $.ajax({
       url: 'https://api.github.com/users/' + username,
       data: {
-        client_id: '024773f2d3eb43f78b29',
-        client_secret: '84209e971855c028d13183e79bc290f3afdcb1a6'
+        client_id: 'b9315bcd5a07fcd759d8',
+        client_secret: 'a2b698bf7e7c02f898197cf136d1a41f704ca8e4'
       }
     }).done(function(user) {
-      //console.log(user);
-      $('#profile').html(`<div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="#" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">$(user.name)</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+      $.ajax({
+        url: 'https://api.github.com/users/' + username + '/repos',
+        data: {
+          client_id: '024773f2d3eb43f78b29',
+          client_secret: '84209e971855c028d13183e79bc290f3afdcb1a6',
+          sort: 'created: asc',
+          per_page: 5
+        }
+      }).done(function(repos) {
+        $.each(repos, function(index, repo) {
+          $('#repos').append(`
+          <div class="card">
+            <div class="row">
+              <div class="col-md-7">
+                <strong>${repo.name}</strong>: ${repo.description}
+              </div>
+              <div class="col-md-3">
+                <span class="badge badge-dark">Forks: ${repo.forks_count}</span>
+                <span class="badge badge-primary">Watchers: ${
+                  repo.watchers_count
+                }</span>
+                <span class="badge badge-success">Stars: ${
+                  repo.stargazers_count
+                }</span>
+              </div>
+              <div class="col-md-2">
+                <a href="${
+                  repo.html_url
+                }" target="_blank" class="btn btn-dark">Repo Page</a>
+              </div>
+            </div>
+          </div>
+        `);
+        });
+      });
+      $('#profile').html(`
+      <div class="card border-primary mb-3" style="max-width: 100rem;">
+        <div class="card-header"><h3>${user.name}</h3></div>
+        <div class="card-body">
+          <div class="row">
+          <div class="col-md-3">
+            <img class="img-thumbnail avatar" src="${user.avatar_url}">
+            <a target="_blank" class="btn btn-primary btn-block" href="${
+              user.html_url
+            }">View Profile</a>
+          </div>
+          <div class="col-md-9">
+            <span class="badge badge-dark">Public Repos: ${
+              user.public_repos
+            }</span>
+            <span class="badge badge-primary">Public Gists: ${
+              user.public_gists
+            }</span>
+            <span class="badge badge-success">Followers: ${
+              user.followers
+            }</span>
+            <span class="badge badge-info">Following: ${user.following}</span>
+            <br><br>
+            <ul class="list-group">
+              <li class="list-group-item">Company: ${user.company}</li>
+              <li class="list-group-item">Website/blog: <a href="${
+                user.blog
+              }" target="_blank">${user.blog}</a></li>
+              <li class="list-group-item">Location: ${user.location}</li>
+              <li class="list-group-item">Member Since: ${user.created_at}</li>
+            </ul>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>`);
+      <h3 class="page-header">Latest Repos</h3>
+      <div id="repos"></div>
+      `);
     });
   });
 });
